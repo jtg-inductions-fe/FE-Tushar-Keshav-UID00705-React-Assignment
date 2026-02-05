cfac9fe0431585b83c7ae2d14e4b09e85f9f0b13
ubuntu <tushar.keshav@joshtechnologygroup.com>
2026-02-05 12:23:11 +0530

    changes fixes

--- src/layout/PublicLayout.tsx
+++ src/layout/PublicLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
+import { Toolbar } from '@mui/material';
 import Grid2 from '@mui/material/Grid2';
 
 import { Header, Sidebar } from '@components';
@@ -8,6 +9,8 @@ export default function PublicLayout() {
     return (
         <Grid2 container>
             <Header></Header>
+            <Toolbar />
+
             <Sidebar></Sidebar>
             <Outlet />
         </Grid2>
