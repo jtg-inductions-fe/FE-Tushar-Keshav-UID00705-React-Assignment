ef207f3a9ecb9d4250311e780aa65715ebda2199
ubuntu <tushar.keshav@joshtechnologygroup.com>
2026-02-06 17:07:32 +0530

    review fixes

--- src/components/ErrorView/ErrorView.constant.ts
+++ src/components/ErrorView/ErrorView.constant.ts
@@ -1,4 +1,2 @@
-import { theme } from '@theme';
-
 // Max width for the images of ErrorView
-export const imgMaxWidth: string = theme.typography.pxToRem(900);
+export const imgMaxWidth: string = '90%';
