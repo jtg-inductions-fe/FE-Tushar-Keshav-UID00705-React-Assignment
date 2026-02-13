01addf02d8f8523638ccc0723ed8542dea1e48fa
ubuntu <tushar.keshav@joshtechnologygroup.com>
2026-02-13 18:35:41 +0530

    Created latest customer and top products sections

--- src/components/sectionCard/SectionCard.tsx
+++ src/components/sectionCard/SectionCard.tsx
@@ -14,6 +14,7 @@ import { SectionCardProps } from './SectionCard.types';
 
 export function SectionCard({
     children,
+    subtext,
     title,
     toolTipText,
 }: PropsWithChildren<SectionCardProps>) {
@@ -23,25 +24,28 @@ export function SectionCard({
     return (
         <Box
             display="flex"
-            gap={theme.spacing(4)}
+            gap={theme.spacing(isMobile ? 4 : 6)}
             flexGrow={1}
             padding={theme.spacing(isMobile ? 4 : 6)}
             flexDirection="column"
             borderRadius={theme.spacing(4)}
             boxShadow={theme.shadows[3]}
         >
-            <Box display="flex" gap={theme.spacing(2)} alignItems="center">
-                <Typography variant="h2">{title}</Typography>
-                {toolTipText && (
-                    <Tooltip title={toolTipText} placement="top">
-                        <IconButton>
-                            <ErrorOutline
-                                fontSize="small"
-                                htmlColor={theme.palette.grey[400]}
-                            />
-                        </IconButton>
-                    </Tooltip>
-                )}
+            <Box>
+                <Box display="flex" gap={theme.spacing(2)} alignItems="center">
+                    <Typography variant="h2">{title}</Typography>
+                    {toolTipText && (
+                        <Tooltip title={toolTipText} placement="top">
+                            <IconButton>
+                                <ErrorOutline
+                                    fontSize="small"
+                                    htmlColor={theme.palette.grey[400]}
+                                />
+                            </IconButton>
+                        </Tooltip>
+                    )}
+                </Box>
+                {subtext && <Typography variant="body2">{subtext}</Typography>}
             </Box>
             {children}
         </Box>
--- src/pages/overview/Dashboard.page.tsx
+++ src/pages/overview/Dashboard.page.tsx
@@ -1,22 +1,79 @@
+import { CardItem } from 'components/cardItem';
 import { QuiltedImageList } from 'components/imageList/ImageList.component';
 import { CustomLineChart } from 'components/lineChart/LineChart.component';
 
-import { Box, Grid2, useTheme } from '@mui/material';
+import {
+    Box,
+    Divider,
+    Grid2,
+    List,
+    useMediaQuery,
+    useTheme,
+} from '@mui/material';
 
 import { SectionCard } from '@components';
 
-import { DATA_POINTS, IMAGE_DATA } from './Dashboard.constants';
+import {
+    DATA_POINTS,
+    IMAGE_DATA,
+    LATEST_CUSTOMERS,
+    TOP_PRODUCTS,
+} from './Dashboard.constants';
 
 export function Dashboard() {
     const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('tablet'));
     return (
         <Grid2 container padding={theme.spacing(4)}>
             <QuiltedImageList images={IMAGE_DATA} />
-            <SectionCard spacing={4} title="Sales" toolTipText="Sales data">
-                <Box width="100%">
-                    <CustomLineChart data={DATA_POINTS} />
+            <Box
+                display="flex"
+                flexDirection="column"
+                minWidth="100%"
+                gap={theme.spacing(4)}
+            >
+                <SectionCard title="Sales" toolTipText="Sales data">
+                    <Box width="100%">
+                        <CustomLineChart data={DATA_POINTS} />
+                    </Box>
+                </SectionCard>
+                <Box
+                    display="flex"
+                    gap={theme.spacing(4)}
+                    flexDirection={isMobile ? 'column' : 'row'}
+                >
+                    <SectionCard title="Latest Customers">
+                        <List>
+                            {LATEST_CUSTOMERS.map((customer, index) => (
+                                <>
+                                    <CardItem key={customer.id} {...customer} />
+                                    {index < TOP_PRODUCTS.length - 1 && (
+                                        <Divider
+                                            color={theme.palette.grey[200]}
+                                            variant="middle"
+                                        />
+                                    )}
+                                </>
+                            ))}
+                        </List>
+                    </SectionCard>
+                    <SectionCard title="Top Products">
+                        <List>
+                            {TOP_PRODUCTS.map((product, index) => (
+                                <>
+                                    <CardItem key={product.id} {...product} />
+                                    {index < TOP_PRODUCTS.length - 1 && (
+                                        <Divider
+                                            color={theme.palette.grey[200]}
+                                            variant="middle"
+                                        />
+                                    )}
+                                </>
+                            ))}
+                        </List>
+                    </SectionCard>
                 </Box>
-            </SectionCard>
+            </Box>
         </Grid2>
     );
 }
